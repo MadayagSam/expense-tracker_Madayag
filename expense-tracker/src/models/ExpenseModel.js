29f2d1c131c71c9353d648430e2562c08127ef30
MadayagSam <158627859+MadayagSam@users.noreply.github.com>
2026-01-24 18:13:41 +0800

    Update ExpenseModel.js

--- expense-tracker/src/models/ExpenseModel.js
+++ expense-tracker/src/models/ExpenseModel.js
@@ -1,1 +1,20 @@
+const db = require("../config/database");
+
+class ExpenseModel {
+  static getAll(callback) {
+    db.all("SELECT * FROM expenses", [], (err, rows) => {
+      callback(err, rows);
+    });
+  }
+
+  static add(title, amount, callback) {
+    db.run(
+      "INSERT INTO expenses (title, amount) VALUES (?, ?)",
+      [title, amount],
+      callback
+    );
+  }
+}
+
+module.exports = ExpenseModel;
 
