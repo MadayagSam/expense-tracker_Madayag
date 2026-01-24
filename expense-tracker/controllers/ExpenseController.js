50f5957f9b4b05999c3bf0b7cd30b16d7a0f23d5
MadayagSam <158627859+MadayagSam@users.noreply.github.com>
2026-01-24 18:38:05 +0800

    Update ExpenseController.js

--- expense-tracker/controllers/ExpenseController.js
+++ expense-tracker/controllers/ExpenseController.js
@@ -1,1 +1,22 @@
+const ExpenseModel = require("../models/ExpenseModel");
+
+class ExpenseController {
+  static getExpenses(req, res) {
+    ExpenseModel.getAll((err, expenses) => {
+      if (err) return res.status(500).json(err);
+      res.json(expenses);
+    });
+  }
+
+  static addExpense(req, res) {
+    const { title, amount } = req.body;
+
+    ExpenseModel.add(title, amount, (err) => {
+      if (err) return res.status(500).json(err);
+      res.status(201).json({ message: "Expense added" });
+    });
+  }
+}
+
+module.exports = ExpenseController;
 
