6f3210b8802866f44d6183e619c3aa5ac142e712
MadayagSam <158627859+MadayagSam@users.noreply.github.com>
2026-01-24 18:13:09 +0800

    Update database.js

--- expense-tracker/config /database.js
+++ expense-tracker/config /database.js
@@ -1,1 +1,21 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./expenses.db", (err) => {
+  if (err) {
+    console.error("Database error:", err.message);
+  } else {
+    console.log("Connected to SQLite database.");
+  }
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS expenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    amount REAL
+  )
+`);
+
+module.exports = db;
+
 
