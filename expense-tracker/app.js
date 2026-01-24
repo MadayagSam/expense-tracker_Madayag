48417db00a8f2f87d06861a9caa2886b93c1d8ab
MadayagSam <158627859+MadayagSam@users.noreply.github.com>
2026-01-24 18:16:48 +0800

    Update app.js

--- expense-tracker/app.js
+++ expense-tracker/app.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const path = require("path");
+const ExpenseController = require("./controllers/ExpenseController");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/api/expenses", ExpenseController.getExpenses);
+app.post("/api/expenses", ExpenseController.addExpense);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
